553996beaad3dfbbf1e51084e3fc712b1e6c0940
Genesis <genesis.castillo52@myhunter.cuny.edu>
2023-11-07 00:45:50 -0500

    adding deck.cpp to hpp

--- Deck.hpp
+++ Deck.hpp
@@ -72,3 +72,94 @@ class Deck
 
 #include "Deck.cpp"
 #endif
+
+
+
+
+ /**
+         * @post: Construct a new Deck object
+         */
+        template<typename CardType>
+        Deck<CardType>::Deck() {
+            cards_; //this vector starts out empty for each object of this class
+        };
+
+        /**
+         * @post: Destroy the Deck object 
+         */
+        template<typename CardType>
+        Deck<CardType>::~Deck() {
+            
+            cards_.clear()
+        };
+
+        /**
+         * @post: Add a Card to the Deck
+         * @param: const reference to CardType card
+         */
+        template<typename CardType>
+        void Deck<CardType>::AddCard(const CardType& card){
+                cards_.insert(card); 
+        };
+
+        /**
+         * @post: Draw a card from the deck
+         * @pre: the deck is not empty
+         * @return the right hand value of type CardType 
+         */
+        template<typename CardType>
+        CardType&& Deck<CardType>::Draw(){
+            if (!IsEmpty()) {
+               CardType&& new_Card = std::move(cards_.back());
+               return std::move(new_Card);
+            }
+        };
+
+        /**
+         * @return if the deck is empty 
+         */
+        template<typename CardType>
+        bool Deck<CardType>::IsEmpty() const{
+            if (cards_.empty()){
+                return true;
+                std::cout << "true" << std::endl;
+            }
+            else 
+                return false;
+                std::cout << "false" << std::endl;
+
+        };
+
+        /**
+         * @post: Shuffle the deck 
+         * Create a random number generator using std::mt19937 with seed 2028358904, 
+         * then call std::shuffle on the vector of cards, and with the random number generator as the third argument.  
+         * https://en.cppreference.com/w/cpp/algorithm/random_shuffle
+         * https://en.cppreference.com/w/cpp/numeric/random/mersenne_twister_engine
+         */
+        template<typename CardType>
+        void Deck<CardType>::Shuffle(){
+
+            std::mt19937 seed_Value;
+            seed_Value.seed(2028358904);
+
+            std::shuffle(cards_.begin(), cards_.end(), seed_Value);
+            
+        };
+
+        /**
+         * @return the size of the deck 
+         */
+        template<typename CardType>
+        int Deck<CardType>::getSize() const {
+                
+                return cards_.size();
+        };
+
+        /**
+         * @return the vector of cards in the deck 
+         */
+        template<typename CardType>
+        std::vector<CardType> Deck<CardType>::getDeck() const {
+            return cards_;
+        };
